52e321285f29808e3826ce22b7d6706a607cc279
AbhijitAher <seabhi.aher@gmail.com>
2022-03-09 02:10:53 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+// ------------------------------- Controllers -------------------------------
 const clinicController = require("./controllers/clinic.controller");
 
+app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
